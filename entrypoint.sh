595c60201200247a2aca9257f3f97d108dc66072
sigec <gesrua@gmail.com>
2021-10-29 22:01:39 +0800

    fix

---
+++ entrypoint.sh
@@ -0,0 +1,2 @@
+node_exporter --collector.disable-defaults --collector.filesystem --collector.cpu --collector.cpufreq --collector.diskstats --collector.meminfo --collector.netdev --collector.netclass &
+/app/lug -c /configs/config.yaml
